5346241f21c5842d94b3751e2bac8884ac6e086e
thinktank-dev1 <wilson@thinktank.co.za>
2023-05-12 06:09:12 +0000

    Dev

--- routes/channels.php
+++ routes/channels.php
@@ -17,5 +17,5 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 Broadcast::channel('auction-chanel', function ($user, $id) {
-    return true;
+    return $user;
 });
